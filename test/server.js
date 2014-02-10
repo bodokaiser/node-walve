5f4c2e1c2ad29309128a158b921d1b78700381b9
Bodo Kaiser <i@bodokaiser.io>
2014-02-10 18:37:04 +0100

    added empty test files for server and socket

---
+++ test/server.js
@@ -0,0 +1,27 @@
+var chai      = require('chai');
+var stream    = require('stream');
+var salvatore = require('../lib');
+
+describe('Server', function() {
+
+  describe('new Server()', function() {
+
+  });
+
+  describe('Event: "connection"', function() {
+
+  });
+
+  describe('Event: "error"', function() {
+
+  });
+
+  describe('Event: "end"', function() {
+
+  });
+
+  describe('server.listen(http)', function() {
+
+  });
+
+});
---
+++ test/socket.js
@@ -0,0 +1,31 @@
+var chai      = require('chai');
+var stream    = require('stream');
+var salvatore = require('../lib');
+
+describe('Socket', function() {
+
+  describe('new Socket(socket)', function() {
+
+  });
+
+  describe('Event: "connect"', function() {
+
+  });
+
+  describe('Event: "message"', function() {
+
+  });
+
+  describe('Event: "close"', function() {
+
+  });
+
+  describe('Event: "error"', function() {
+
+  });
+
+  describe('Event: "end"', function() {
+
+  });
+
+});
