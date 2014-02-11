cf52282c87642ef1c879434d73ff48b261f6ae1a
Bodo Kaiser <i@bodokaiser.io>
2014-02-11 17:32:18 +0100

    added implementation of Incoming

--- test/incoming.js
+++ test/incoming.js
@@ -4,15 +4,41 @@ var salvatore = require('../lib');
 
 describe('Incoming', function() {
 
+  var incoming;
+
   describe('new Incoming()', function() {
 
   });
 
   describe('Event: "readable"', function() {
 
+    beforeEach(function() {
+      incoming = new salvatore.Incoming();
+    });
+
+    it('should be emitted with "Hello"', function(done) {
+      incoming.once('readable', function() {
+        var result = incoming.read().toString();
+
+        chai.expect(result).to.equal('Hello');
+      });
+      incoming.write(new Buffer([0x81, 0x05]));
+      incoming.write(new Buffer('Hello'));
+    });
+
   });
 
   describe('Event: "end"', function() {
+
+    beforeEach(function() {
+      incoming = new salvatore.Incoming();
+    });
+
+    xit('should be emitted on payload end', function(done) {
+      incoming.once('end', done);
+
+      incoming.write(new Buffer([0x81, 0x00]));
+    });
 
   });
 
