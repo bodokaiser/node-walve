f7c11c02175d425586549f7af5a1b24f448abc74
Bodo Kaiser <i@bodokaiser.io>
2014-02-11 10:46:49 +0100

    updated server implementation

--- test/server.js
+++ test/server.js
@@ -1,29 +1,44 @@
 var chai      = require('chai');
 var http      = require('http');
-var stream    = require('stream');
+var events    = require('events');
 var salvatore = require('../lib');
 
 describe('Server', function() {
 
-  var wsServer, httpServer;
-
-  before(function() {
-    httpServer = new http.Server().listen(3000);
-  });
-
-  beforeEach(function() {
-    wsServer = new salvatore.Server();
-  });
-
   describe('new Server([options])', function() {
+
+    it('should return instance of EventEmitter', function() {
+      var server = new salvatore.Server();
+
+      chai.expect(server).to.be.an.instanceOf(events.EventEmitter);
+    });
 
   });
 
   describe('#listen(server)', function() {
 
+    it('should listen to the servers "upgrade" event', function() {
+      var server = new http.Server();
+      var wserver = new salvatore.Server();
+
+      wserver.listen(server);
+
+      chai.expect(server.listeners('upgrade')).to.have.length(1);
+    });
+
   });
 
   describe('Event: "connect"', function() {
+
+    var server, wserver;
+
+    beforeEach(function() {
+      server = new http.Server();
+      wserver = new salvatore.Server();
+
+      server.listen(3000);
+      wserver.listen(server);
+    });
 
     it('should be emitted on successful http upgrade', function(done) {
       http.request({
@@ -36,19 +51,16 @@ describe('Server', function() {
         port: 3000
       }).end();
 
-      wsServer.once('connect', function() {
-        done();
-      });
-      wsServer.listen(httpServer);
+      wserver.once('connect', done);
+    });
+
+    afterEach(function() {
+      server.close();
     });
 
   });
 
   describe('Event: "error"', function() {
-
-  });
-
-  describe('Event: "end"', function() {
 
   });
 
