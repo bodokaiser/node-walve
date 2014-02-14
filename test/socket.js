fc5e275b4afb6d2f06d3a6fe647414774e11065f
Bodo Kaiser <i@bodokaiser.io>
2014-02-14 16:16:02 +0100

    implemented Socket readable

--- test/socket.js
+++ test/socket.js
@@ -17,11 +17,19 @@ describe('Socket', function() {
 
   describe('Event: "message"', function() {
 
-    it('should be emitted once on single incoming', function(done) {
+    it('should be emitted with incoming', function(done) {
       socket.once('message', function(incoming) {
         chai.expect(incoming).to.be.instanceOf(wesos.Incoming);
 
+        done();
+      });
+      source.write(new Buffer([0x81, 0x05]));
+    });
+
+    it('should be emitted at single incoming', function(done) {
+      socket.once('message', function(incoming) {
         var result = '';
+
         incoming.on('readable', function() {
           result += incoming.read().toString();
         });
@@ -31,6 +39,34 @@ describe('Socket', function() {
           done();
         });
       });
+      source.write(new Buffer([0x81]));
+      source.write(new Buffer([0x05]));
+      source.write(new Buffer([0x48]));
+      source.write(new Buffer([0x65]));
+      source.write(new Buffer([0x6c]));
+      source.write(new Buffer([0x6c]));
+      source.write(new Buffer([0x6f]));
+    });
+
+    it('should be emitted at double incoming', function(done) {
+      var counter = 0;
+
+      socket.on('message', function(incoming) {
+        var result = '';
+
+        incoming.on('readable', function() {
+          result += incoming.read().toString();
+        });
+        incoming.on('end', function() {
+          chai.expect(result).to.equal('Hello');
+
+          if (counter) done();
+
+          counter++;
+        });
+      });
+      source.write(new Buffer([0x81, 0x05]));
+      source.write(new Buffer('Hello'));
       source.write(new Buffer([0x81, 0x05]));
       source.write(new Buffer('Hello'));
     });
